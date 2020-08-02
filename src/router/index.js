88aceee60965bd1d30b220f1b6757089f4defdb9
吴杨 <xiaolvshiwuyang@qq.com>
2020-08-02 15:43:18 +0800

    登录

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,19 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login'
+
 Vue.use(VueRouter)
 
 const router = new VueRouter({
   routes: [
-    { path: '/', redirect: '/login' },
-    { path: '/login', component: Login }
+    {
+      path: '/',
+      redirect: '/login'
+    },
+    {
+      path: '/login',
+      component: Login
+    }
   ]
 })
 
